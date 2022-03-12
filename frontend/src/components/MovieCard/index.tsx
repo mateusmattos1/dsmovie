ee6d1dde84eda4fec4d877aef3d5a751e80d6b0c
Mateus de Mattos <mateusmattos327@gmail.com>
2022-03-11 22:27:17 -0300

    Show Score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,11 +1,10 @@
-import MovieScore from "../MovieScore";
+import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
 
 type Props = {
     movie: Movie;
 }
-
 
 function MovieCard( { movie } : Props) {
 
@@ -14,7 +13,7 @@ function MovieCard( { movie } : Props) {
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
+                <MovieScore count={movie.count} score={movie.score} />
 
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
@@ -22,7 +21,7 @@ function MovieCard( { movie } : Props) {
 
             </div>
         </div>
-    )
+    );
 }
 
 export default MovieCard;
